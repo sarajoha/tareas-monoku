768519834d836784e9326e0846de86e2ca543762
sarajoha <sjcc333@gmail.com>
2019-04-11 17:05:11 -0500

    Agregue validacion de datos

--- julian.py
+++ julian.py
@@ -66,19 +66,17 @@ arr1 = [2, 5, 43, 6, 76, 100, 4, 43, 3,]
 #     print(letra + ' esta en la frase')
 
 #homework
-#programa que genere numero aleatorio y el usuario pone un numero, da un numero de intentos
-#debe adivinar, o 'el numero es mas alto', 'el numero es mas bajo'
 #bonus, cuantos intentos para que no gane siempre
 
 def dificultad(nivel):
 
-    if nivel == 'facil':
+    if nivel == '0':
         print('Tendras 8 intentos para adivinar un numero entre 0 y 300')
         return 8, 300
-    elif nivel == 'medio':
+    elif nivel == '1':
         print('Tendras 5 intentos para adivinar un numero entre 0 y 600')
         return 5, 600
-    elif nivel == 'dificil':
+    elif nivel == '2':
         print('Tendras 2 intentos para adivinar un numero entre 0 y 1000')
         return 2, 1000
     else:
@@ -86,9 +84,21 @@ def dificultad(nivel):
         return 0
 
 
+def isInt(string):
+    """
+    check whether the input can be transformed into an integer
+    """
+    try:
+        int(string)
+    except ValueError:
+        return False
+
+    return True
+
+
 def adivinar():
 
-    nivel = input('Ingrese el nivel de dificultad: ')
+    nivel = input('Escoja el nivel de dificultad, siendo 0 facil, 1 medio y 2 dificil: ')
 
     nivel_dif, max_num = dificultad(nivel)
 
@@ -97,7 +107,17 @@ def adivinar():
     count = 0
 
     while count < nivel_dif:
-        num_usuario = int(input('Ingrese un numero: '))
+        num_usuario = (input('Ingrese un numero: '))
+        if isInt(num_usuario):
+            num_usuario = int(num_usuario)
+        else:
+            print('El input debe ser un numero')
+            continue
+
+        if num_usuario < 0 or num_usuario > max_num:
+            print('Este numero no esta en el rango!')
+            continue
+
         if num_usuario == num_random:
             print('Adivinaste!')
             break
@@ -107,6 +127,7 @@ def adivinar():
             print('Intenta con numeros mas bajos')
 
         count = count + 1
+
         if count == (nivel_dif - 1):
             print('Ultima oportunidad')
 
