e7c08f824fd80082d1b4c0ea7b2816c574820ed2
sarajoha <sjcc333@gmail.com>
2019-04-22 17:39:07 -0500

    Termine ejemplos de modelos de django

---
+++ clase_jose.py
@@ -0,0 +1,21 @@
+new_dict = {'llave1': 'hola', 'llave2': [1, 3, 5, 97], 'llave3': {'key1': 1, 'key2': 2, 'key3': 3}}
+
+llaves = ['']
+
+print(new_dict['llave3']['key2'])
+
+new_dict['llave2'].append(55)
+
+print(new_dict['llave2'])
+
+new_dict['llave4'] = 'otro valor'
+
+print(new_dict)
+
+new_dict['llave3']['key4'] = 4
+
+print(new_dict['llave3'])
+
+print(sorted(new_dict['llave3']))
+
+#averiguar quien es otros y grupos, permisos de carpetas
--- example_models.py
+++ example_models.py
@@ -1,33 +1,36 @@
 #import models
 
-class Cliente(models.Model):
-    saldo = FloatField()
-    limiteCredito =
-    descuento =
-    #manytomany con Direccion
-    #one to many con pedido PedidoCabecera
 
+class Cliente(models.Model):
+    saldo = models.FloatField()
+    limite_credito = models.PositiveIntegerField(validators=[MaxValueValidator(3000000)])
+    descuento = models.FloatField()
+    #direccion = models.ManyToManyField(Direccion)
 
 class Direccion(models.Model):
-    numero = IntegerField()
-    calle = CharField()
-    comuna = CharField()
-    ciudad = charField()
-
-
-class PedidoCabecera(models.Model):
-    fecha = DateField()
-    #one to many con PedidoCuerpo
-
-
-class PedidoCuerpo(models.Model):
-    cantidad = IntegerField()
+    numero = models.IntegerField()
+    calle = models.CharField(max_length=30)
+    comuna = models.CharField(max_length=20)
+    ciudad = models.charField(max_length=20)
+    #cliente = models.ManyToManyField(Cliente)
 
 
 class Fabrica(models.Model):
-    nombre = CharField()
-    tlfContacto = IntegerField()
-    #one to many articulos
+    nombre = models.CharField(max_length=50)
+    tlf_contacto = models.PositiveIntegerField(max_length=10, validators=[MinLengthValidator(10)]) #minimo cifra de x numeros
+
 
 class Articulo(models.Model):
     descripcion = CharField()
+    fabrica = models.ForeignKey(Fabrica, on_delete=models.PROTECT))
+
+
+class PedidoCuerpo(models.Model):
+    cantidad = models.PositiveIntegerField()
+    articulo = models.ForeignKey(Articulo, on_delete=models.PROTECT)
+
+
+class PedidoCabecera(models.Model):
+    fecha = models.DateTimeField(auto_now=True)
+    pedidoCuerpo = models.ManyToManyField(PedidoCuerpo)
+    cliente = models.ForeignKey(Cliente, on_delete=models.PROTECT))
